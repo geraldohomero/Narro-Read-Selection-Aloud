72955469dc423b2cbe0bde7be336cf20144df85f
geraldohomero <geraldohomero@protonmail.com>
2026-07-11 17:33:04 -0300

    style: Clean up reader page layout margins and styles

--- narro_rsa/reader_page.py
+++ narro_rsa/reader_page.py
@@ -10,7 +10,8 @@ import gi
 
 gi.require_version("Gdk", "4.0")
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gdk, GLib, Gtk
+gi.require_version("Adw", "1")
+from gi.repository import Gdk, GLib, Gtk, Adw
 
 from narro_rsa.clipboard import get_clipboard_text
 from narro_rsa.constants import (
